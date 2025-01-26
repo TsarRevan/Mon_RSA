9f1c3372a36139e8d363ddd71612525ef3e346f4
TsarRevan <thomas.rahari@gmail.com>
2025-01-26 23:41:09 +0100

    Premier commit

---
+++ rsa.py
@@ -0,0 +1,25 @@
+import sys
+import math         
+import random
+
+def display_help():
+##Pensez a rentrer un message pour le menu help
+    print("""Menu Cauet Burger
+""")
+    
+def is_prime(num):
+    
+    if num <=1:
+        return False
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def generate_prime(minimum, maximum):
+
+    while True:
+        num = random.randint(minimum, maximum)
+        if is_prime(num):
+            return num
+    
